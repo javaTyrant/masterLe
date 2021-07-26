8aceb3fbb9cc74a8ebd1c0c60d804956790e237f
lufengxiang <lufengxiang@Znlh.com>
2021-07-26 09:11:46 +0800

    'init'

--- src/masterKth/Day0723.java
+++ src/masterKth/Day0723.java
@@ -52,21 +52,24 @@ public class Day0723 {
         return ans;
     }
 
+    //小顶堆.
     public int[] topKFrequentHeap(int[] nums, int k) {
         Map<Integer, Integer> occurrences = new HashMap<>();
         for (int num : nums) {
             occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
         }
-        // int[] 的第一个元素代表数组的值，第二个元素代表了该值出现的次数
+        // int[] 的第一个元素代表数组的值，第二个元素代表了该值出现的次数.
         PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(m -> m[1]));
         for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
             int num = entry.getKey(), count = entry.getValue();
+            //
             if (queue.size() == k) {
+                //
                 if (queue.peek()[1] < count) {
                     queue.poll();
                     queue.offer(new int[]{num, count});
                 }
-            } else {
+            } else {//
                 queue.offer(new int[]{num, count});
             }
         }
@@ -82,4 +85,61 @@ public class Day0723 {
     //数据流中的第 K 大元素
 
     //最接近原点的 K 个点
+
+    //1063. 有效子数组的数目:暴力法.
+    public int validSubArrays(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] >= nums[i]) {
+                    result++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    @Test
+    public void testCon() {
+        System.out.println(confusingNumber(69));
+    }
+
+    public static boolean confusingNumber(int n) {
+        char[] cs = String.valueOf(n).toCharArray();
+        for (char c : cs) {
+            if (!is(c - 48)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean is(int n) {
+        return n == 1 || n == 0 || n == 6 || n == 8 || n == 9;
+    }
+
+    @Test
+    public void testPurchasePlans() {
+
+    }
+
+    //LCP 28. 采购方案
+    public int purchasePlans(int[] nums, int target) {
+        int left, right;
+        int n = 0;
+        Arrays.sort(nums);
+        for (left = 0, right = nums.length - 1; left < right; ) {
+            if (nums[left] + nums[right] > target) right--;
+            else {
+                n = n + right - left;
+                left++;
+            }
+        }
+        return n % 1000000007;
+    }
 }
